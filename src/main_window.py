abeaeb6a04b2ad0120afb4c3ebe110d4f845c0f2
Mykyta Shumskyi <nish41255@gmail.com>
2024-08-08 16:56:09 +0300

    Added a method and changed the label widget

--- src/main_window.py
+++ src/main_window.py
@@ -1,11 +1,9 @@
 import sys
-import numpy as np
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QLabel, QRadioButton,
-    QPushButton, QWidget, QMessageBox, QSpinBox, QLineEdit, QTableWidget,
-    QVBoxLayout, QHBoxLayout, QGridLayout
+    QPushButton, QWidget, QMessageBox, QVBoxLayout
 )
-from PyQt6.QtGui import QFont, QPalette, QColor
+from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
@@ -20,15 +18,10 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout(self.central_widget)
 
-        layout.addStretch(1)
-
-
         self.label = QLabel('Choose the kind of problem', self.central_widget)
         self.label.setFont(QFont('Arial', 20))
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
         layout.addWidget(self.label)
-
-        layout.addStretch(1)
 
 
         self.radio_uncertainty = QRadioButton('Decision making under uncertainty', self.central_widget)
@@ -37,14 +30,25 @@ class MainWindow(QMainWindow):
         layout_radio = QVBoxLayout()
         layout_radio.addWidget(self.radio_uncertainty)
         layout_radio.addWidget(self.radio_risk)
-        layout.addLayout(layout_radio)
 
+        layout_radio.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        layout.addLayout(layout_radio)
         self.button_next = QPushButton('Next', self.central_widget)
+        self.button_next.clicked.connect(self.on_button_click)
         layout.addWidget(self.button_next)
 
-
-
-
+    def on_button_click(self):
+        if self.radio_uncertainty.isChecked():
+            task = 'Decision making under uncertainty'
+        elif self.radio_risk.isChecked():
+            task = 'Decision making under risk'
+        else:
+            QMessageBox.warning(self, 'Choosing the task', 'Choose the kind of problem')
+            return
+        
+        self.new_window = DimensionWindow(task)
+        self.new_window.show()
 
 app = QApplication(sys.argv)
 
