7ae636040be3cb09b0b0decfacf81bbaf285b05d
Mykyta Shumskyi <nish41255@gmail.com>
2024-08-21 16:27:56 +0300

    Updated criteria to resolve test failures

--- src/decision_making_under_uncertainty.py
+++ src/decision_making_under_uncertainty.py
@@ -4,25 +4,46 @@ class DecisionMakingUnderUncertainty:
     
     @staticmethod
     def wald_criterion(payoff_matrix):
+        if payoff_matrix.ndim == 1 and payoff_matrix.shape[0] != 0:
+            return 1
         min_row = np.min(payoff_matrix, axis=1)
-        solution = np.argmax(min_row) + 1
-        return solution
+        solution = np.flatnonzero(min_row == np.max(min_row)) + 1
+        if solution.size == 1:
+            return solution[0]
+        else:
+            return list(solution)
 
     @staticmethod
     def laplace_criterion(payoff_matrix):
+        if payoff_matrix.ndim == 1 and payoff_matrix.shape[0] != 0:
+            return 1
         mean_row = np.mean(payoff_matrix, axis=1)
-        solution = np.argmax(mean_row) + 1
-        return solution
+        solution = np.flatnonzero(mean_row == np.max(mean_row)) + 1
+        if solution.size == 1:
+            return solution[0]
+        else:
+            return list(solution)
 
     @staticmethod
     def maximax_criterion(payoff_matrix):
+        if payoff_matrix.ndim == 1 and payoff_matrix.shape[0] != 0:
+            return 1
         max_row = np.max(payoff_matrix, axis=1)
-        solution = np.argmax(max_row) + 1
-        return solution
+        solution = np.flatnonzero(max_row == np.max(max_row)) + 1
+        if solution.size == 1:
+            return solution[0]
+        else:
+            return list(solution)
 
     @staticmethod
     def hurwitz_criterion(payoff_matrix, alpha):
+        if payoff_matrix.ndim == 1 and payoff_matrix.shape[0] != 0:
+            return 1
         min_row = alpha * np.min(payoff_matrix, axis=1)
         max_row = (1 - alpha) * np.max(payoff_matrix, axis=1)
-        solution = np.argmax(min_row + max_row) + 1
-        return solution
+        combined = min_row + max_row
+        solution = np.flatnonzero(combined == np.max(combined)) + 1
+        if solution.size == 1:
+            return solution[0]
+        else:
+            return list(solution)
